be77ae17ff15e417cc16af5ae905d0a0a15a9e4a
Richard Darst <rkd@zgib.net>
2018-03-24 14:32:58 +0200

    setup_tree.sh: Import this from previous location outside of git

---
+++ setup_tree.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+WRKDIR=/scratch/work/$USER
+# Create Jupyter home tree
+JHOME=$WRKDIR/.jupyterhub-tree
+mkdir -p $JHOME
+chmod u+w $JHOME
+# Add some useful links
+test -e $JHOME/home       || ln -sT $HOME    $JHOME/home
+test -e $JHOME/work       || ln -sT $WRKDIR  $JHOME/work
+test -e $JHOME/scratch    || ln -sT /scratch $JHOME/scratch
+test -e $JHOME/m          || ln -sT /m       $JHOME/m
+test -e $JHOME/filesystem || ln -sT /        $JHOME/filesystem
+# Make this directory non-modifiable
+chmod u-w,g-w,o-w $JHOME
+# Clean up old spawner logfiles
+find ~/ -maxdepth 1 -regextype egrep -regex '.*jupyterhub_slurmspawner_[0-9]+.log' -mtime 7 -delete
