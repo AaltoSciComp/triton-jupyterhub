5a5ee69730cc40e443b72989df6cb987435e9428
Richard Darst <rkd@zgib.net>
2019-06-19 11:11:26 +0300

    Batchspawner: update for batchspawner/singleuser commands done properly

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -69,6 +69,8 @@ c.JupyterHub.cookie_options = dict(secure=True)
 #
 import batchspawner
 c.JupyterHub.spawner_class = 'wrapspawner.ProfilesSpawner'
+c.SlurmSpawner.batchspawner_wrapper='{BASEDIR}/miniconda/bin/batchspawner-singleuser'.format(BASEDIR=BASEDIR)
+
 c.Spawner.http_timeout = 120
 # Slurm options can be found at https://github.com/jupyterhub/batchspawner/blob/master/batchspawner/batchspawner.py#L78
 prologue = """\
@@ -76,7 +78,7 @@ set -x
 echo $PATH
 unset XDG_RUNTIME_DIR
 sh {BASEDIR}/setup_tree.sh""".format(BASEDIR=BASEDIR)
-cmd = "{BASEDIR}/miniconda/bin/python -E -s {BASEDIR}/miniconda/bin/batchspawner-singleuser --FileContentsManager.delete_to_trash=False".format(BASEDIR=BASEDIR)
+cmd = "{BASEDIR}/miniconda/bin/jupyterhub-singleuser --FileContentsManager.delete_to_trash=False".format(BASEDIR=BASEDIR)
 
 slurm_default = dict(req_partition='interactive', req_options='', req_workdir='/scratch/work/{user}',
                      cmd=cmd, req_nprocs='2',
